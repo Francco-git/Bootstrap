17e05d655fa0acf4743d3959018920aaed5d1af9
Francco-git <70703749+Francco-git@users.noreply.github.com>
2020-10-27 14:26:45 -0300

    Finalização do site

---
+++ script.js
@@ -0,0 +1,17 @@
+let mapa = document .querySelector('#mapa')
+
+
+function mapaZoom(){
+    mapa.style.widht = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal(){
+    mapa.style.widht = '600px'
+    mapa.style.height = '450px'
+
+}
+
+function enviar(){
+    alert('Mensagem enviada com sucesso')
+}
